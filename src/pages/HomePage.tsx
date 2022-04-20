6be18452ab56ac8fc975790b4e0d57d151f3f639
DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>
2022-04-20 18:28:14 +0200

    feat: add new files (Routes and Register) + Create Route manager
    Signed-off-by: DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import { Text } from '@chakra-ui/react';
+
+const HomePage = (): JSX.Element => (
+	<>
+		<Text>Home</Text>
+	</>
+);
+
+export default HomePage;
---
+++ src/pages/Register.tsx
@@ -0,0 +1,9 @@
+import { Text } from '@chakra-ui/react';
+
+const Register = (): JSX.Element => (
+	<>
+		<Text>Register</Text>
+	</>
+);
+
+export default Register;
